348f3c3fceb1f7114b31b08b1e61a2f2fb80356d
BartheqVonKoss <bartek.kos@live.com>
2020-09-21 08:46:12 +0900

    explored data, prepared dataset loader

---
+++ src/cfg.py
@@ -0,0 +1,4 @@
+class Configuration:
+
+    training_folder = '/home/bartheq/Documents/playground/image_colorization/data/training_images/'
+    validation_folder = '/home/bartheq/Documents/playground/image_colorization/data/validation_images/'
